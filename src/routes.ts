7403134cdae664aa6bbf1069b33ed8fa7a8e67e6
charlesdasilvacosta <dasilvacosta.charles@gmail.com>
2018-08-02 01:20:53 +0200

    Add tsconfig

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import registerCtrl from './controllers/register/register.controller';
+import express from "express";
 
 export class Routes {
-    public routes(app): void {
+    public routes(app: express.Application): void {
         app.get('/', registerCtrl.test);
     }
 }
